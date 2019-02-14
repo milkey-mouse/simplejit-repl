22497bb8ba610b8020351bd14816b6c7e54a6974
Milkey Mouse <milkeymouse@meme.institute>
2019-02-13 16:27:13 -0800

    Handle Ctrl-C interrupt in infinite loops

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
 extern crate cranelift;
 extern crate cranelift_module;
 extern crate cranelift_simplejit;
+extern crate libc;
+extern crate nix;
 extern crate rustyline;
+extern crate signal_hook;
 
-use std::{io, io::Read, mem, process};
+use std::{mem, panic, ptr, sync::mpsc, thread};
 
 use rustyline::{error::ReadlineError, Editor};
+use signal_hook::iterator::Signals;
 
 mod frontend;
 mod jit;
 
 use frontend::parser;
 
+#[derive(Debug)]
+enum EvalMsg {
+    IAmThread(libc::pthread_t),
+    Done(isize),
+    Signaled(libc::c_int),
+}
+
 fn main() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let signals = Signals::new(&[signal_hook::SIGINT])
+            .expect("ctrl-c handler failed, you won't be able to terminate loops");
+        loop {
+            for signal in signals.pending() {
+                tx1.send(EvalMsg::Signaled(signal));
+            }
+        }
+    });
+
     // Create the JIT instance, which manages all generated functions and data.
     let mut jit = jit::JIT::new();
 
@@ -43,6 +66,7 @@ fn main() {
                         parser::function(assembled_fn.as_ref())
                     {
                         new_function.clear();
+
                         if let Err(e) = jit.compile_from_parsed(name, params, the_return, stmts) {
                             eprintln!("compile error: {}", e);
                         }
@@ -62,63 +86,50 @@ fn main() {
 
                     let compiled = unsafe { mem::transmute::<_, fn() -> isize>(compiled) };
 
-                    println!("{}", compiled());
+                    let tx2 = mpsc::Sender::clone(&tx);
+                    let thread = thread::spawn(move || {
+                        let thread_id = unsafe { libc::pthread_self() };
+                        tx2.send(EvalMsg::IAmThread(thread_id)).unwrap();
+
+                        unsafe {
+                            signal_hook::register(signal_hook::SIGUSR1, || unsafe {
+                                libc::pthread_exit(ptr::null_mut::<libc::c_void>());
+                            })
+                            .unwrap();
+                        }
+
+                        let ret = compiled();
+                        tx2.send(EvalMsg::Done(ret)).unwrap();
+                    });
+
+                    let mut thread_id = None;
+                    for msg in rx.iter() {
+                        match msg {
+                            EvalMsg::IAmThread(id) => thread_id = Some(id),
+                            EvalMsg::Done(ret) => {
+                                println!("{}", ret);
+                                thread.join().unwrap();
+                                break;
+                            }
+                            EvalMsg::Signaled(_) => {
+                                if let Some(id) = thread_id {
+                                    unsafe {
+                                        libc::pthread_kill(id, signal_hook::SIGUSR1 as i32);
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                    }
                 } else {
                     eprintln!("syntax error");
                 }
 
                 lineno += 1;
-
-                //match parser::function_definition(line.as_ref()) {
-                //    Ok((name, params, the_return, stmts)) => { jit
-                //        .compile_from_parsed(name, params, the_return, stmts)
-                //        .unwrap_or_else(|msg| {
-                //            eprintln!("error: {}", msg);
-                //            process::exit(1);
-                //        }); },
-                //    Err(parser::ParseError { expected, .. }) => { println!("expected: {:?}", expected) },
-                //    Err(e) => {}
-                //}
-
-                //} else if let Ok(parsed) = parser::statement(line.as_ref()) {
-                //    if lines_buf
-                //}
-                //match parser::function(line.as_ref()) {
-                //    Ok(parsed) => {}
-                //    Err(e) => println!("fn err: {:?}", e),
-                //}
-                //if let Ok(stmt) = parser::statement(line.as_ref()) {
-                //    if lines_buf.len() == 0 {
-                //        println!("running");
-                //        // just run
-                //    } else {
-                //        lines_buf.push(line);
-                //        println!("{}", lines_buf.join("\n"));
-                //    }
-                //}
             }
             Err(ReadlineError::Interrupted) => {}
             Err(ReadlineError::Eof) => break,
             Err(e) => println!("rl err: {:?}", e),
         }
     }
-
-    process::exit(0);
-
-    let mut code = String::new();
-    {
-        let stdin = io::stdin();
-        let mut handle = stdin.lock();
-        handle.read_to_string(&mut code);
-    }
-
-    let func = jit.compile(&code).unwrap_or_else(|msg| {
-        eprintln!("error: {}", msg);
-        process::exit(1);
-    });
-
-    let func = unsafe { mem::transmute::<_, fn() -> isize>(func) };
-
-    let ret = func();
-    process::exit(ret as i32);
 }
