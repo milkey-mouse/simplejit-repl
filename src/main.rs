79e8695a83e828625ddbb30e4e34a2d5078822f5
Milkey Mouse <milkeymouse@meme.institute>
2019-02-13 18:16:28 -0800

    Use better parsing logic
    Instead of having a separate function header to parse, handle any multiline
    construct by continuing as long as the parse error is because the statement is
    unfinished (i.e. at EOF).

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate nix;
 extern crate rustyline;
 extern crate signal_hook;
 
-use std::{mem, panic, ptr, sync::mpsc, thread};
+use std::{mem, ptr, sync::mpsc, thread};
 
 use rustyline::{error::ReadlineError, Editor};
 use signal_hook::iterator::Signals;
@@ -31,7 +31,7 @@ fn main() {
             .expect("ctrl-c handler failed, you won't be able to terminate loops");
         loop {
             for signal in signals.pending() {
-                tx1.send(EvalMsg::Signaled(signal));
+                tx1.send(EvalMsg::Signaled(signal)).unwrap();
             }
         }
     });
@@ -40,14 +40,10 @@ fn main() {
     let mut jit = jit::JIT::new();
 
     let mut lineno = 0;
-    let mut new_function = Vec::<String>::new();
+    let mut statement = Vec::<String>::new();
     let mut rl = Editor::<()>::new();
     loop {
-        let readline = rl.readline(if new_function.len() == 0 {
-            ">>> "
-        } else {
-            "... "
-        });
+        let readline = rl.readline(if statement.len() == 0 { ">>> " } else { "... " });
         match readline {
             Ok(line) => {
                 if line.chars().all(char::is_whitespace) {
@@ -57,72 +53,81 @@ fn main() {
                 rl.add_history_entry(line.as_ref());
 
                 let line = line + "\n";
+                statement.push(line);
 
-                if new_function.len() != 0 {
-                    new_function.push(line);
-                    let assembled_fn = new_function.join("");
+                let assembled_statement = statement.join("");
+                match parser::statement(assembled_statement.as_ref()) {
+                    Ok(_) => {
+                        statement.clear();
 
-                    if let Ok((name, params, the_return, stmts)) =
-                        parser::function(assembled_fn.as_ref())
-                    {
-                        new_function.clear();
-
-                        if let Err(e) = jit.compile_from_parsed(name, params, the_return, stmts) {
-                            eprintln!("compile error: {}", e);
-                        }
-                    }
-                } else if let Ok(_) = parser::function_definition(line.as_ref()) {
-                    new_function.push(line);
-                } else if let Ok(_) = parser::statement(line.as_ref()) {
-                    let anon_fn = format!("fn __line{}() -> (x) {{\nx = {}}}\n", lineno, line);
-
-                    let compiled = match jit.compile(anon_fn.as_ref()) {
-                        Ok(x) => x,
-                        Err(e) => {
-                            eprintln!("compile error: {}", e);
-                            continue;
-                        }
-                    };
-
-                    let compiled = unsafe { mem::transmute::<_, fn() -> isize>(compiled) };
-
-                    let tx2 = mpsc::Sender::clone(&tx);
-                    let thread = thread::spawn(move || {
-                        let thread_id = unsafe { libc::pthread_self() };
-                        tx2.send(EvalMsg::IAmThread(thread_id)).unwrap();
-
-                        unsafe {
-                            signal_hook::register(signal_hook::SIGUSR1, || unsafe {
-                                libc::pthread_exit(ptr::null_mut::<libc::c_void>());
-                            })
-                            .unwrap();
-                        }
-
-                        let ret = compiled();
-                        tx2.send(EvalMsg::Done(ret)).unwrap();
-                    });
-
-                    let mut thread_id = None;
-                    for msg in rx.iter() {
-                        match msg {
-                            EvalMsg::IAmThread(id) => thread_id = Some(id),
-                            EvalMsg::Done(ret) => {
-                                println!("{}", ret);
-                                thread.join().unwrap();
+                        let anon_fn = format!(
+                            "fn __line{}() -> (x) {{\nx = {}}}\n",
+                            lineno, assembled_statement
+                        );
+                        let compiled = match jit.compile(anon_fn.as_ref()) {
+                            Ok(x) => x,
+                            Err(e) => {
+                                eprintln!("compile error: {}", e);
                                 break;
                             }
-                            EvalMsg::Signaled(_) => {
-                                if let Some(id) = thread_id {
-                                    unsafe {
-                                        libc::pthread_kill(id, signal_hook::SIGUSR1 as i32);
-                                    }
+                        };
+                        let compiled = unsafe { mem::transmute::<_, fn() -> isize>(compiled) };
+
+                        let tx2 = mpsc::Sender::clone(&tx);
+                        let thread = thread::spawn(move || {
+                            let thread_id = unsafe { libc::pthread_self() };
+                            tx2.send(EvalMsg::IAmThread(thread_id)).unwrap();
+
+                            unsafe {
+                                signal_hook::register(signal_hook::SIGUSR1, || {
+                                    libc::pthread_exit(ptr::null_mut::<libc::c_void>());
+                                })
+                                .unwrap();
+                            }
+
+                            let ret = compiled();
+                            tx2.send(EvalMsg::Done(ret)).unwrap();
+                        });
+
+                        let mut thread_id = None;
+                        for msg in rx.iter() {
+                            match msg {
+                                EvalMsg::IAmThread(id) => thread_id = Some(id),
+                                EvalMsg::Done(ret) => {
+                                    println!("{}", ret);
+                                    thread.join().unwrap();
+                                    break;
                                 }
-                                break;
+                                EvalMsg::Signaled(_) => {
+                                    if let Some(id) = thread_id {
+                                        unsafe {
+                                            libc::pthread_kill(id, signal_hook::SIGUSR1 as i32);
+                                        }
+                                    }
+                                    break;
+                                }
                             }
                         }
                     }
-                } else {
-                    eprintln!("syntax error");
+                    Err(ref e) if e.offset == assembled_statement.len() => {}
+                    Err(_) => match parser::function(assembled_statement.as_ref()) {
+                        Ok((name, params, the_return, stmts)) => {
+                            statement.clear();
+                            if let Err(e) = jit.compile_from_parsed(name, params, the_return, stmts)
+                            {
+                                eprintln!("compile error: {}", e);
+                            }
+                        }
+                        Err(ref e) if e.offset == assembled_statement.len() => {}
+                        Err(e) => {
+                            statement.clear();
+                            eprintln!(
+                                "syntax error (offset {} len {})",
+                                e.offset,
+                                assembled_statement.len()
+                            );
+                        }
+                    },
                 }
 
                 lineno += 1;
